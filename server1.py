cfcfcad43b57622173e3d9dc8d06144e63c44801
yamikarajput <yamika.rajput@knoldus.com>
2021-10-27 18:52:53 +0530

    file uploaded

---
+++ server1.py
@@ -0,0 +1,10 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello from server1'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5001)
